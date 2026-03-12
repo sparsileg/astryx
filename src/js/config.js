263d47b5019ae105f98a47b6cc5d139eccb56695
sparsileg <github@sparsile.org>
2026-03-12 03:34:37 -0400

    Incremented version number to
    15

--- src/js/config.js
+++ src/js/config.js
@@ -11,7 +11,7 @@
 const APP_CONFIG = {
     APP_NAME: 'Astryx',
     APP_TITLE: 'Astryx - Astrophotography Planning Tool',
-    APP_VERSION: '0.14.0',
+    APP_VERSION: '0.15.0',
     DB_NAME: 'astryx-db',
     DB_VERSION: 8,
     TARGET_DATA_PATH: './data/',
